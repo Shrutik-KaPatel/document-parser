84a4375cf8c768aad6ed7c82feb3dbcc46ebec03
Shrutik-KaPatel <ka.patelshrutik37@gmail.com>
2026-04-13 07:49:07 -0400

    feat: Fix word extraction to stop at period delimiter

--- document_parser.c
+++ document_parser.c
@@ -6,15 +6,15 @@
 #define MAX_PARAGRAPHS 5
 
 char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
-
+ return document[n-1][m-1][k-1];
 }
 
 char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) { 
-
+   return document[k-1][m-1];
 }
 
 char*** kth_paragraph(char**** document, int k) {
-
+ return document[k-1];
 }
 
 char**** get_document(char* text) {
@@ -64,7 +64,7 @@ char**** get_document(char* text) {
         {
         int letter_count = 1;
         char* word_begin = word_start;
-        while (*word_start != ' ' && *word_start != '\0') {
+        while (*word_start != ' ' && *word_start != '.' && *word_start != '\0') {
             letter_count++;
             word_start++;
         }
